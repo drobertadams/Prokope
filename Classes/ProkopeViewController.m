08fb7dc16ae0e1df91b0faad6b3ea70c2eb89769
Robert Adams <d.robert.adams@gmail.com>
2011-05-16 15:07:36 -0400

    Minor changes.

--- Classes/ProkopeViewController.m
+++ Classes/ProkopeViewController.m
@@ -30,26 +30,6 @@
 
 
 
-/*
- // Implement loadView to create a view hierarchy programmatically, without using a nib.
- - (void)loadView {
- }
- */
-
-/*
-// The designated initializer. Override to perform setup that is required before the view is loaded.
-- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil {
-    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
-    if (self) {
-        // Custom initialization
-    }
-    return self;
-}
-*/
-
-
-
-
 
 
 
--- Classes/WebViewController.m
+++ Classes/WebViewController.m
@@ -1,4 +1,4 @@
-    //
+//
 //  WebViewController.m
 //  Prokope
 //
