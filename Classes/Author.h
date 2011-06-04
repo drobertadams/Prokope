b03c12897f2f2b2fcf323d3ad977db8842817492
Justin Antranikian <justin.antranikian@gmail.com>
2011-06-03 20:59:46 -0400

    Added new files

---
+++ Classes/Author.h
@@ -0,0 +1,24 @@
+//
+//  Author.h
+//  Prokope
+//
+//  Created by Justin Antranikian on 6/3/11.
+//  Copyright 2011 Grand Valley State University. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface Author : NSObject {
+	
+	NSString *name;
+	NSString *iconURL;
+	NSMutableArray *WorksArray;
+	
+}
+
+@property (nonatomic, retain) NSString *name;
+@property (nonatomic, retain) NSString *iconURL;
+@property (nonatomic, retain) NSMutableArray *WorksArray;
+
+@end
---
+++ Classes/Author.m
@@ -0,0 +1,14 @@
+//
+//  Author.m
+//  Prokope
+//
+//  Created by Justin Antranikian on 6/3/11.
+//  Copyright 2011 Grand Valley State University. All rights reserved.
+//
+
+#import "Author.h"
+
+
+@implementation Author
+
+@end
