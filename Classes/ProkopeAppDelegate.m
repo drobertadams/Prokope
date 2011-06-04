e8fc11cbe3e01e5c0c74731c0c2d7d21f276f23c
Justin Antranikian <justin.antranikian@gmail.com>
2011-06-03 20:46:28 -0400

    XML additions to the project

--- Classes/ProkopeAppDelegate.m
+++ Classes/ProkopeAppDelegate.m
@@ -20,7 +20,21 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {    
     
+	
     // Override point for customization after app launch.
+	
+	AuthorsArray = [[NSMutableArray alloc] initWithCapacity:100];
+	
+	AuthorCount = 0;							
+	WorkCount = 0;
+	
+	NSString* path = [[NSBundle mainBundle] pathForResource: @"Authors" ofType: @"xml"];
+	NSData* data = [NSData dataWithContentsOfFile: path];
+	NSXMLParser* parser = [[NSXMLParser alloc] initWithData: data];
+	
+	[parser setDelegate:self];
+	[parser parse];
+	[parser release];
 	
 	// This line initialized the navigation controller. 
 	ProkopeNavigationController = [[UINavigationController alloc] init];
@@ -28,15 +42,82 @@
 	[window addSubview:ProkopeNavigationController.view];
 	
 	viewController = [[ProkopeViewController alloc] initWithNibName:@"ProkopeViewController" bundle:nil];
-
+	[viewController SetDataArray:AuthorsArray];
+	
 	[ProkopeNavigationController pushViewController:viewController animated:NO];
 	[viewController release];
 	
-	// Set the view controller as the window's root view controller and display.
-    //self.window.rootViewController = self.viewController;
     [self.window makeKeyAndVisible];
-
+	
 	return YES;
+}
+
+- (void)parser:(NSXMLParser *)parser didStartElement:(NSString *)elementName 
+  namespaceURI:(NSString *)namespaceURI qualifiedName:(NSString *)qualifiedName 
+	attributes:(NSDictionary *)attributeDict {
+	
+	
+	if([elementName isEqualToString:@"Author"])
+	{		
+		WorkCount = 0;
+		
+	//	Author *newAuthor = [[Author alloc] init];
+		NSString *thename = [attributeDict objectForKey:@"name"];
+		NSString *icon = [attributeDict objectForKey:@"icon"];
+		
+	//	newAuthor.name = thename;
+	//	newAuthor.iconURL = icon;
+		
+	//	[AuthorsArray addObject:newAuthor];
+		
+	}
+	else if ([elementName isEqualToString:@"Work"])
+	{	
+		NSString *url = [attributeDict objectForKey:@"url"];
+		NSString *name = [attributeDict objectForKey:@"name"]; 
+		
+	//	Work *w = [[Work alloc] init];
+	//	w.name = name;
+	//	w.workURL = url;
+		
+	//	Author *a = [AuthorsArray objectAtIndex:AuthorCount];
+	//	[a.WorksArray addObject:w];
+	//	[w release];
+	}
+	else if ([elementName isEqualToString:@"Chapter"])
+	{	
+		NSString *name = [attributeDict objectForKey:@"name"];
+		NSString *url = [attributeDict objectForKey:@"url"]; 
+		
+		// This is the key to setting up the array. The counts are incremented as the XML file is being parsed.
+		// That serves as a place holder to know where to insert the 'Chapters'.
+	//	Author *a = [AuthorsArray objectAtIndex:AuthorCount];
+	//	Work *w = [a.WorksArray objectAtIndex:WorkCount];
+		
+	//	Work *newOne = [[Work alloc] init];
+	//	newOne.name = name;
+	//	newOne.workURL = url;
+		
+	//	[w.ChaptersArray addObject:newOne];
+	}
+}
+
+- (void)parser:(NSXMLParser *)parser didEndElement:(NSString *)elementName 
+  namespaceURI:(NSString *)namespaceURI qualifiedName:(NSString *)qName
+{
+	if([elementName isEqualToString:@"Prokope"])
+	{
+		NSLog(@"Done Parsing Prokope");
+	}
+	else if ([elementName isEqualToString:@"Author"])
+	{
+		AuthorCount ++;
+	}
+	else if ([elementName isEqualToString:@"Work"])
+	{
+		WorkCount ++;
+	}
+	
 }
 
 
